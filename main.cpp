ef0645b82a19f8f7b378266726824b4d3180ac4f
Chelsey_Leonia <1-d_x_d-1@mail.ru>
2022-04-20 17:15:18 +0300

    cwk

--- main.cpp
+++ main.cpp
@@ -1,4 +1,7 @@
 #include <iostream>
+#include <math.h>
+#include <string>
+#include <fstream>
 
 using namespace std;
 
@@ -12,7 +15,9 @@ enum e_target
 
 class TTarget
 {
-protected:
+public:
+
+	double _x, _y;
 
 	TTarget()
 	{
@@ -29,14 +34,19 @@ protected:
 		_t = 0;
 	}
 
-	virtual void Move(double ti) = 0;
+	virtual void Move(double ti)
+	{
+		ti = 0;
+	}
 
 	~TTarget()
 	{
 		cout << "Destructor from TTarget colled" << endl;
 	}
 
-	double _x0, _y0, _V, _K, _x, _y, _t;
+protected:
+
+	double _x0, _y0, _V, _K, _t;
 	int _type;
 };
 
@@ -56,11 +66,13 @@ public:
 		this->_type = AIRCRAFT;
 	}
 
-	void Move(double ti)
+	virtual void Move(double ti)
 	{
 		_x = _x0 - _V * cos(_K) * (ti - _t);
 		_y = _y0 - _V * sin(_K) * (ti - _t);
 	}
+
+	~TAircraft() {}
 
 };
 
@@ -80,11 +92,13 @@ public:
 		this->_type = MISSILE;
 	}
 
-	void Move(double ti)
+	virtual void Move(double ti)
 	{
 		_x = _x0 - (_V + _N * (ti - _t)) * cos(_K) * (ti - _t);
 		_y = _y0 - (_V + _N * (ti - _t)) * sin(_K) * (ti - _t);
 	}
+
+	~TMissile() {}
 
 private:
 
@@ -93,26 +107,42 @@ private:
 
 class RLS
 {
+public:
+
 	RLS()
 	{
 		cout << "Defaul constructor from RLS colled" << endl;
 	}
 
-	RLS(double x, double y, double R, TTarget **targets)
-		: _x(x), _y(y), _R(R), _targets(targets)
+	RLS(double x, double y, double R, TTarget *targets, int tarLen)
+		: _x(x), _y(y), _R(R), _targets(targets), _tarLen(tarLen)
 	{
 		cout << "Param constructor from RLS colled" << endl;
 	}
 
 	void Peleng(double t0, double tk)
 	{
+		std::ofstream	fout("result");
+		if (!fout)
+		{
+			std::cout << "file open error";
+			return ;
+		}
+
 		for (int t = t0; t < tk; t += DT)
 		{
-			for (int i = 0; _targets[i] != NULL; i++)
+			for (int i = 0; i < _tarLen; i++)
 			{
-				
+				_targets[i].Move(t);
+				double D = sqrt(pow(_targets[i]._x - _x, 2) + pow(_targets[i]._y - _y, 2));
+				if (D <= _R)
+				{
+					double Az0 = atan((_targets[i]._y - _y) / (_targets[i]._x - _x));
+					fout << t << ") " << D << " ; " << Az0 << endl;
+				}
 			}
 		}
+		fout.close();
 	}
 
 	~RLS()
@@ -120,14 +150,31 @@ class RLS
 		cout << "Destructor from RLS colled" << endl;
 	}
 
+private:
+
 	double _x, _y, _R;
-	TTarget **_targets;
+	int _tarLen;
+	TTarget *_targets;
 };
 
 int main( void )
 {
+	/*
+	_plaNum++;
+	TLA **newPLA = new TLA* [_plaNum];
+	for (int i = 0; i < _plaNum - 1; i++)
+		newPLA[i] = _PLA[i];
+	delete [] _PLA;
+	newPLA[_plaNum - 1] = newTLA;
+	_PLA = newPLA;
+	*/
 
+	TTarget *targets = new TTarget [2];
+	targets[0] = TAircraft(1, 1, 20, 5);
+	targets[1] = TMissile(2, 2, 30, 6, 1);
 
+	RLS rls(1, 2, 3, targets, 2);
+	rls.Peleng(0, 10);
 
 	return (0);
 }
